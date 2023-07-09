e445566aa61d8a71857fb024ed6f8a35cf93f83f
Ceejay-777 <covenantcrackslord01@gmail.com>
2023-07-09 19:23:02 +0100

    commit

--- src/Typography.jsx
+++ src/Typography.jsx
@@ -6,9 +6,12 @@ const Typography = () => {
         <h1 className='text-3xl italic '>Title 1</h1>
         <h2 className='text-2xl'>Title 2</h2>
         <h3 className='text-xl'>Title 3</h3>
-        <p className='text-base underline decoration-orange-500 decoration-wavy decoration-2 underline-offset-4'>A regular paragraph</p>
-        <p className='text-sm'>A description paragraph</p>
+        <p className='text-base underline decoration-orange-500 decoration-wavy decoration-2 underline-offset-4  leading-loose'>A regular paragraph</p>
+        <p className='text-sm capitalize'>a description paragraph</p>
         <p className="note text-xs">A little note</p> 
+        <div style={{width: "200px"}}>
+          <p className='truncate'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa consectetur minima rerum neque, at pariatur magni porro molestiae esse eius tenetur reprehenderit eaque incidunt suscipit ad! Fuga adipisci illum illo aut quisquam doloribus consequuntur, saepe perspiciatis velit, nulla necessitatibus corrupti.</p>
+        </div>
     </div> 
   )
 }
