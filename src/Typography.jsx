c60d8a6191c13f2bfdf24b9ec7068828e733b06f
Ceejay-777 <covenantcrackslord01@gmail.com>
2023-07-09 18:49:49 +0100

    commit

--- src/Typography.jsx
+++ src/Typography.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 const Typography = () => {
   return (
     <div>
-        <h1 className='text-3xl'>Title 1</h1>
+        <h1 className='text-3xl italic '>Title 1</h1>
         <h2 className='text-2xl'>Title 2</h2>
         <h3 className='text-xl'>Title 3</h3>
-        <p className='text-base'>A regular paragraph</p>
+        <p className='text-base underline decoration-orange-500 decoration-wavy decoration-2 underline-offset-4'>A regular paragraph</p>
         <p className='text-sm'>A description paragraph</p>
         <p className="note text-xs">A little note</p> 
     </div> 
