4228adfb7a7f6f709cfb0ca0525da355fd7370f6
Ceejay-777 <covenantcrackslord01@gmail.com>
2023-07-10 21:17:43 +0100

    commit

--- src/SpacesAndSizes.jsx
+++ src/SpacesAndSizes.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const SpacesAndSizes = () => {
   return (
     <div>
-        <div className='bg-red-500 text-white font-bold ml-4'>Box with margin
+        {/* <div className='bg-red-500 text-white font-bold ml-4'>Box with margin
         </div>
         <div className='bg-red-500 text-white font-bold p-4'>Box with padding</div>
         <br />
@@ -16,10 +16,12 @@ const SpacesAndSizes = () => {
 
         <div class="w-36 h-16 bg-blue-500 opacity-20"></div>
         <div class="bg-red-300 -mt-8">-mt-8</div>
-        <br />
+        <br /> */}
 
-        <div className="w-16 h-16 bg-red-500"></div>
-        <div className="w-16 h-16 bg-green-500"></div>
+        <div className="flex min-h-screen space-x-16">
+            <div className="w-1/5 bg-red-500 ">Sidebar</div>
+            <div className="w-4/5 bg-green-500">Main Content</div>
+        </div>
     </div>
   )
 }
