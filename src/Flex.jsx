4c44ffd8be5746f48e62f09ab7610664f8e3cca1
Ceejay-777 <covenantcrackslord01@gmail.com>
2023-07-11 21:06:47 +0100

    commit

--- src/Flex.jsx
+++ src/Flex.jsx
@@ -17,15 +17,34 @@ const Flex = () => {
                 <div>Another Sidebar</div>
             </div>
         </div> */}
-        <div>
+        
+        {/* <div>
             Header
         </div>
         <div className='flex flex-col md:flex-row'>
-            {/* <div className='md:w-32 bg-sky-500'>Sidebar</div>
-            <div className='w-full bg-red-600'>Main Content</div> */}
-            <div>01</div>
-            <div>02</div>
-            <div>03</div>
+            <div className='md:w-32 bg-sky-500'>Sidebar</div>
+            <div className='w-full bg-red-600'>Main Content</div>
+            <div className='flex flex-wrap'>
+                <div className='w-32 grow'>02</div>
+                <div className='w-32 grow'>01</div>
+                <div className='w-32 grow'>03</div>
+                <div className='w-32 grow'>04</div>
+                <div className='w-32 grow'>05</div>
+            </div>
+        </div> */}
+
+        <div>Header</div>
+        <div className="flex  items-baseline">
+            {/* <div>Sidebar</div>
+            <div>
+                <h1 className='font-bold'>Main Content</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque tenetur ullam in deserunt, temporibus optio qui eos assumenda labore, cum similique a. Voluptatum vitae provident molestiae natus distinctio repudiandae et.</p>
+            </div>
+            <div>Another Sidebar</div> */}
+
+            <div className="pt-8 pb-12">01</div>
+            <div className="pt-2 pb-6">02</div>
+            <div className="pt-12 pb-4">03</div>
         </div>
     </div>
   )
