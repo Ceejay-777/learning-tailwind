0c1be8dc51308a61a153d80273b61596f099daf5
Ceejay-777 <covenantcrackslord01@gmail.com>
2023-07-15 20:28:27 +0100

    commit

--- src/Layouts.jsx
+++ src/Layouts.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from "react";
+import pic from "./assets/faq.jpeg";
 
 const Layouts = () => {
   return (
@@ -8,24 +9,58 @@ const Layouts = () => {
     //     </div>
     // </div>
 
-    <div className='h-screen text-white Parent bg-slate-900'>
-        <div className="container px-4 columns-3 columns-lg">
-           <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis facilis nobis fugit ipsa iste saepe, labore quod laboriosam aliquam eius, odio reiciendis voluptatum quisquam? At a quas corrupti fugiat quam.
-           </p>
-           <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis facilis nobis fugit ipsa iste saepe, labore quod laboriosam aliquam eius, odio reiciendis voluptatum quisquam? At a quas corrupti fugiat quam.
-           </p>
-           <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis facilis nobis fugit ipsa iste saepe, labore quod laboriosam aliquam eius, odio reiciendis voluptatum quisquam? At a quas corrupti fugiat quam.
-           </p>
-           <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis facilis nobis fugit ipsa iste saepe, labore quod laboriosam aliquam eius, odio reiciendis voluptatum quisquam? At a quas corrupti fugiat quam.
-           </p> 
+    // <div className='h-screen text-white Parent bg-slate-900'>
+    //     <div className="container px-4 columns-3 columns-lg">
+    //        <p>
+    //         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis facilis nobis fugit ipsa iste saepe, labore quod laboriosam aliquam eius, odio reiciendis voluptatum quisquam? At a quas corrupti fugiat quam.
+    //        </p>
+    //        <p>
+    //         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis facilis nobis fugit ipsa iste saepe, labore quod laboriosam aliquam eius, odio reiciendis voluptatum quisquam? At a quas corrupti fugiat quam.
+    //        </p>
+    //        <p>
+    //         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis facilis nobis fugit ipsa iste saepe, labore quod laboriosam aliquam eius, odio reiciendis voluptatum quisquam? At a quas corrupti fugiat quam.
+    //        </p>
+    //        <p>
+    //         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis facilis nobis fugit ipsa iste saepe, labore quod laboriosam aliquam eius, odio reiciendis voluptatum quisquam? At a quas corrupti fugiat quam.
+    //        </p>
+    //     </div>
+    // </div>
 
+    // <div className="h-screen text-white Parent bg-slate-900">
+    //   <div className="container px-4 text-sm">
+    //     <h1 className="hidden">Hello</h1>
+    //     <img src={pic} alt="CeeJay" className="w-48 mx-4 rounded-md float-left" />
+    //     <img src={pic} alt="CeeJay" className="w-36 mx-4 rounded-md float-right" />
+    //     <p className="clear-right">
+    //       Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis
+    //       facilis nobis fugit ipsa iste saepe, labore quod laboriosam aliquam
+    //       eius, odio reiciendis voluptatum quisquam? At a quas corrupti fugiat
+    //       quam.
+    //     </p>
+    //     <p>
+    //       Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis
+    //       facilis nobis fugit ipsa iste saepe, labore quod laboriosam aliquam
+    //       eius, odio reiciendis voluptatum quisquam? At a quas corrupti fugiat
+    //       quam.
+    //     </p>
+    //     <p>
+    //       Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis
+    //       facilis nobis fugit ipsa iste saepe, labore quod laboriosam aliquam
+    //       eius, odio reiciendis voluptatum quisquam? At a quas corrupti fugiat
+    //       quam.
+    //     </p>
+    //   </div>
+    // </div>
+
+   <div className='h-screen text-white Parent bg-slate-900'>
+        <div className="container px-4 font-bold">
+            <div className="relative bg-red-500 rounded-lg p-8 w-32 h-32">
+                <div className="w-24 h-24 bg-green-500 rounded-lg absolute"></div>
+            </div>
         </div>
     </div>
-  )
-}
 
-export default Layouts
+  );
+};
+
+export default Layouts;
